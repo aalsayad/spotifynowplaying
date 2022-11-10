f7b2d9f855d97afcb1ac3f47c346c5d8d56bce70
aalsayad <74960405+aalsayad@users.noreply.github.com>
2022-11-10 10:34:33 +0300

    fixed authentication from local storage

--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
+//Variable Dev/Produciton
+local_uri = "http://127.0.0.1:5500/index.html"
+prod_uri = "https://aalsayad.github.io/spotifynowplaying/"
+
+//Spotify App Consts
 const client_id = `a474c8e6511146f0b4b162dc31c55728`;
 const client_secret = `0a64c27e140f4fd99a525f67aa2c7967`;
-const redirect_uri = `https://aalsayad.github.io/spotifynowplaying/`;
 
 
+//Auth token Vars
 let access_token = null;
 let refresh_token = null;
+const redirect_uri = prod_uri;
 
 
+//On page load/Redirect get the code and fetch access token
+const onPageLoad = () => {
+ console.log("App Started (Page rendered)")
+ if (window.location.search.length>0){
+  console.log("Handling Redirect")
+  const queryString = window.location.search;
+  const code = queryString.slice(6)
+  fetchAccessToken(code)
+  window.history.pushState("","", redirect_uri);
+ }
+ else if(localStorage.getItem("access_token")!==null || localStorage.getItem("refresh_token")!==null) {
+  access_token = localStorage.getItem("access_token");
+  refresh_token = localStorage.getItem("refresh_token");
+  setInterval(fetchTrackInformation, 1000);
+ } else {
+  console.log("no refresh/access token | Please re-authenticate with spotify")
+  console.log(`access_token: ${access_token} | `, `refresh_token: ${refresh_token}`)
+ }
+}
 
-//First Authenticate with Spotify to get code
+//Authentication start to spotify platform
 const authToSpotify = () => {
   console.log("Authenticating")
   localStorage.setItem("client_id=", client_id)
@@ -23,26 +48,7 @@ const authToSpotify = () => {
   window.location.href = url;
 }
 
-//On page load/Redirect get the code and fetch access token
-const onPageLoad = () => {
- console.log("pageLoaded")
- access_token = localStorage.getItem("access_token")
- refresh_token = localStorage.getItem("access_token")
- if (window.location.search.length>0){
-  const queryString = window.location.search;
-  const code = queryString.slice(6)
-  fetchAccessToken(code)
-  window.history.pushState("","", redirect_uri);
- } else if ( !access_token || !refresh_token) {
-  console.log("no refresh/access token please relogin")
-  console.log(access_token, refresh_token)
- } else {
-  access_token = localStorage.getItem("access_token")
-  refresh_token = localStorage.getItem("refresh_token")
-  setInterval(fetchTrackInformation, 1000);
- }
-}
-
+//Preparing body to send to spotify using XMLHTTPRequest
 const fetchAccessToken = (code) => {
   let body = "grant_type=authorization_code";
   body += "&code=" + code;
@@ -53,6 +59,7 @@ const fetchAccessToken = (code) => {
   callAuthorizationAPI(body)
 }
 
+//Getting the Access_Token & Refresh_Token
 const callAuthorizationAPI =  (body) =>{
   let xhr = new XMLHttpRequest();
   xhr.open("POST", "https://accounts.spotify.com/api/token", true)
@@ -63,11 +70,9 @@ const callAuthorizationAPI =  (body) =>{
     if(xhr.readyState === 4){
       if(xhr.status == 200){
         let data = JSON.parse(xhr.responseText)
-        console.log(data)
         if (data.access_token != undefined) {
           access_token=data.access_token
           localStorage.setItem("access_token", access_token)
-          // console.log(access_token)
         }
         if(data.refresh_token != undefined){
           refresh_token = data.refresh_token;
@@ -76,16 +81,15 @@ const callAuthorizationAPI =  (body) =>{
         onPageLoad();
         setInterval(fetchTrackInformation, 1000);
       } else {
-        // console.log(xhr.responseText)
         alert(xhr.responseText)
       }
     }
   }
 }
 
-
+//Fetching currently track information from spotify
 const fetchTrackInformation = async () => {
-  console.log("fetching track")
+  console.log(" - Fetching Track Information")
   const result = await fetch(
     `https://api.spotify.com/v1/me/player/currently-playing?market=ES`,
     {
@@ -98,30 +102,29 @@ const fetchTrackInformation = async () => {
 
   if (result.status === 200) {
     const data = await result.json();
-
     const trackDetails = {
       songImage: data.item.album.images[0].url,
       songName: data.item.name,
       artist: data.item.artists[0].name,
       currentDuration: convertMsToTime(data.progress_ms),
       totalDuration: convertMsToTime(data.item.duration_ms),
-      progressBar: (data.progress_ms / data.item.duration_ms) * 100
+      progressBar: (data.progress_ms / data.item.duration_ms) * 100,
+      isPlaying: data.is_playing
     };
 
     renderInformation(trackDetails);
   } else if (result.status === 401) {
+    console.log(`${result}`)
     localStorage.setItem("access_token", null)
     localStorage.setItem("refresh_token", null)
-    // console.log(access_token, refresh_token)
     onPageLoad();
-    // console.log(result);
   }
 };
 
+//Convert Song durations from ms to double digits
 const padTo2Digits = (num) => {
   return num.toString().padStart(2, "0");
 };
-
 const convertMsToTime = (milliseconds) => {
   let seconds = Math.floor(milliseconds / 1000);
   let minutes = Math.floor(seconds / 60);
@@ -132,6 +135,7 @@ const convertMsToTime = (milliseconds) => {
   return `${padTo2Digits(minutes)}:${padTo2Digits(seconds)}`;
 };
 
+//Render Song information
 const renderInformation = (trackDetails) => {
   const divSongImage = document.querySelector(".current-song-img");
   const divSongName = document.querySelector(".current-song-name");
@@ -148,7 +152,7 @@ const renderInformation = (trackDetails) => {
   divSongProgressBar.style.width = trackDetails.progressBar + "%";
 };  
 
-//Render Theme Change
+//Dark/Light UI Theme Change
 const handleThemeChange = () => {
   let darkModeActive = false;
   const darkModeButton = document.getElementById('theme-btn')
